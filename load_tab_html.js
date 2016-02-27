b2a339d8855f6aeb7218da87212f8787a0525c4c
Danny Williams <danny.williams@eadbox.com>
2016-02-27 14:26:29 -0300

    add jquery for loading tabs as external html

---
+++ load_tab_html.js
@@ -0,0 +1,5 @@
+$(function(){
+     $("#overview").load("http://localhost:8000/overview.html");
+     $("#skills").load("http://localhost:8000/skills.html");
+     $("#contact").load("http://localhost:8000/contact.html");
+});
