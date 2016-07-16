0e31b8cd46401a98a49c98f63ef88d13f6caa3da
danny williams <danny.h.williams@hotmail.com>
2016-07-16 20:51:18 +0100

    load tab fix... again

--- load_tab_html.js
+++ load_tab_html.js
@@ -1,15 +1,17 @@
 $(function(){
      cur_url = window.location.href;
-     new_url = cur_url;
-     url_length = cur_url.length;
-     if (cur_url[url_length -1] != '/'){
-       cur_url = cur_url + '/';
-       if (cur_url.indexOf("home.html") > -1 ){
-         end = cur_url.indexOf("home.html");
-         new_url = cur_url.substring(0,end);
-       }
+     if (cur_url.indexOf("home.html") > -1 ){
+       end = cur_url.indexOf("home.html");
+       cur_url = cur_url.substring(0,end);
      }
-     $("#overview").load(new_url+"overview.html");
-     $("#skills").load(new_url+"skills.html");
-     $("#contact").load(new_url+"contact.html");
+     if (cur_url[cur_url.length -1] == '#'){
+       cur_url = cur_url.substring(0, cur_url.length -1);
+     }
+     if (cur_url[cur_url.length-1] != '/'){
+       alert(cur_url);
+       cur_url = cur_url + '/';
+     }
+     $("#overview").load(cur_url+"overview.html");
+     $("#skills").load(cur_url+"skills.html");
+     $("#contact").load(cur_url+"contact.html");
 });
