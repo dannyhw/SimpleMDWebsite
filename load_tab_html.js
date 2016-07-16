fc7fd3e34baad7797ce1d6029f150ebf2714561b
danny williams <danny.h.williams@hotmail.com>
2016-07-16 20:30:16 +0100

    fix terrible yellow and fix for local load tab

--- load_tab_html.js
+++ load_tab_html.js
@@ -1,6 +1,17 @@
 $(function(){
-     cur_url = window.location.href
-     $("#overview").load(cur_url+"overview.html");
-     $("#skills").load(cur_url+"skills.html");
-     $("#contact").load(cur_url+"contact.html");
+     cur_url = window.location.href;
+     new_url = cur_url;
+     url_length = cur_url.length;
+     if (cur_url[url_length -1] != '/'){
+       cur_url = cur_url + '/';
+       start = url_length - 9; end = url_length;
+       sub = cur_url.substring(start, end);
+       if (sub == "home.html" ){
+         new_url = cur_url.substring(0,start);
+       }
+      //  alert(sub + " " + url_length + " " + cur_url + " " + new_url)
+     }
+     $("#overview").load(new_url+"overview.html");
+     $("#skills").load(new_url+"skills.html");
+     $("#contact").load(new_url+"contact.html");
 });
